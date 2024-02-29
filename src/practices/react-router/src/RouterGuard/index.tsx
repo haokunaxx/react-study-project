fc8a31195d37590c0831cd0d2b3d05bd24a8a45b
haokunaxx <xux9801@163.com>
2024-02-29 21:38:42 +0800

    '换电脑临时提交'

--- src/practices/react-router/src/RouterGuard/index.tsx
+++ src/practices/react-router/src/RouterGuard/index.tsx
@@ -4,26 +4,26 @@ import {
   Switch,
   Prompt,
   withRouter,
-  type RouteComponentProps
-} from 'react-router-dom'
-import { Link } from '../Common/LinkWithStyle'
-import { useEffect, useState } from 'react'
+  type RouteComponentProps,
+} from "react-router-dom";
+import { Link } from "../Common/LinkWithStyle";
+import { useCallback, useEffect, useState } from "react";
 
 import {
   navigationBlockStrategy,
-  type CommonRouterProps
-} from './navigationBlockStrategy'
+  type CommonRouterProps,
+} from "./navigationBlockStrategy";
 
-const Home = () => <h1>Home Page</h1>
+const Home = () => <h1>Home Page</h1>;
 
 const Form = () => {
   return (
     <div>
       <p>name: </p>
-      <input type='text' />
+      <input type="text" />
     </div>
-  )
-}
+  );
+};
 const User = () => {
   return (
     <>
@@ -31,115 +31,106 @@ const User = () => {
       <hr />
       <Form />
     </>
-  )
-}
-
+  );
+};
 const Private = () => {
-  return <h1>This is Private Page</h1>
-}
+  return <h1>This is Private Page</h1>;
+};
 
 /*
   vue beforeRouter
 */
 interface RouterGuardProps extends RouteComponentProps {
-  when: boolean
-  navigate: (...params: any[]) => void
-  // FIXME: nextLocation的类型
+  when: boolean;
+  navigate: (
+    commonRouterProps: CommonRouterProps,
+    nextLocation: CommonRouterProps["location"],
+    prevLocation: CommonRouterProps["location"]
+  ) => void;
   shouldBlockNavigation: (
-    nextLocation: CommonRouterProps['location'],
-    prevLocation: CommonRouterProps['location']
-  ) => boolean | string | void
-  title?: string
-  message?: string
-  cancelBtnText?: string
-  confirmBtnText?: string
+    nextLocation: CommonRouterProps["location"],
+    prevLocation: CommonRouterProps["location"]
+  ) => boolean | string | void;
+  title?: string;
+  message?: string;
+  cancelBtnText?: string;
+  confirmBtnText?: string;
 }
-// type RouteGuardProps = PropsWithChildren<{
-//   when: boolean
-//   navigate: (...params: any[]) => void
-//   // FIXME: nextLocation的类型
-//   shouldBlockNavigation: (nextLocation: any) => boolean | string | void
-//   title?: string
-//   message?: string
-//   cancelBtnText?: string
-//   confirmBtnText?: string
-// }>
 
 const modalWrapperStyle = {
-  position: 'fixed' as any,
-  left: '0',
-  top: '0',
-  width: '100%',
-  height: '100%',
+  position: "fixed" as any,
+  left: "0",
+  top: "0",
+  width: "100%",
+  height: "100%",
   zIndex: 99,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)'
-}
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+};
 const modalStyle = {
-  border: '1px solid #ddd',
-  padding: '12px',
-  backgroundColor: '#fff'
-}
+  border: "1px solid #ddd",
+  padding: "12px",
+  backgroundColor: "#fff",
+};
 /*
   false => rerender => something operation => true => navigation
 */
 const RouterGuard = withRouter((props: RouterGuardProps) => {
-  console.log(props)
-  const { location, history, match } = props
-  const { when, shouldBlockNavigation, navigate } = props
+  const { location, history, match } = props;
+  const { when, shouldBlockNavigation, navigate } = props;
   const {
-    title = '',
-    message = 'Are you sure you want to leave?',
-    cancelBtnText = 'cancel',
-    confirmBtnText = 'confirm'
-  } = props
-  const [modalVisible, setModalVisible] = useState(false)
-  const [allowNavigation, setAllowNavigation] = useState(false)
+    title = "",
+    message = "Are you sure you want to leave?",
+    cancelBtnText = "cancel",
+    confirmBtnText = "confirm",
+  } = props;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [allowNavigation, setAllowNavigation] = useState(false);
   const [nextLocation, setNextLocation] = useState<
-    Record<string, any> | undefined
-  >()
+    CommonRouterProps["location"] | undefined
+  >();
   const handleBlockNavigation = (nextLocation: any) => {
     if (!allowNavigation && shouldBlockNavigation(nextLocation, location)) {
       // something operation which needs to update the value of allowNavigation
-      setNextLocation(nextLocation)
-      setModalVisible(true)
+      setNextLocation(nextLocation);
+      setModalVisible(true);
       // Block
-      return false
+      return false;
     }
-    return true
-  }
+    return true;
+  };
 
   const cancelBtnClickHandler = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
 
   const confirmBtnClickHandler = () => {
-    setModalVisible(false)
-    setAllowNavigation(true)
-  }
+    setModalVisible(false);
+    setAllowNavigation(true);
+  };
 
   const RouterGuardConfirmModal = () => (
-    <div className='router-guard-block-modal-wrapper' style={modalWrapperStyle}>
-      <div className='router-guard-block-modal' style={modalStyle}>
-        {title && <div className='modal-title'>{title}</div>}
-        <div className='modal-body'>{message}</div>
-        <div className='modal-footer'>
+    <div className="router-guard-block-modal-wrapper" style={modalWrapperStyle}>
+      <div className="router-guard-block-modal" style={modalStyle}>
+        {title && <div className="modal-title">{title}</div>}
+        <div className="modal-body">{message}</div>
+        <div className="modal-footer">
           <button onClick={cancelBtnClickHandler}>{cancelBtnText}</button>
           <button onClick={confirmBtnClickHandler}>{confirmBtnText}</button>
         </div>
       </div>
     </div>
-  )
+  );
 
   useEffect(() => {
     if (allowNavigation && nextLocation) {
-      navigate({ location, history, match }, nextLocation, location)
-      setNextLocation(undefined)
-      setAllowNavigation(false)
+      navigate({ location, history, match }, nextLocation, location);
+      setNextLocation(undefined);
+      setAllowNavigation(false);
     }
-  }, [allowNavigation, navigate, nextLocation, location, history, match])
+  }, [allowNavigation, navigate, nextLocation, location, history, match]);
 
   return (
     <>
@@ -149,12 +140,21 @@ const RouterGuard = withRouter((props: RouterGuardProps) => {
       {/* <Prompt when={when} message='hello' />   */}
       {modalVisible && <RouterGuardConfirmModal />}
     </>
-  )
-})
+  );
+});
 
 export const RouterGuardDemo = () => {
-  const [whenState, setWhenState] = useState(true)
-
+  // const [whenState, setWhenState] = useState(true);
+  const navigateCallback = useCallback<RouterGuardProps["navigate"]>(
+    ({ history }, nextLocation, curLocation) => {
+      if (curLocation.pathname !== nextLocation.pathname) {
+        history.push(nextLocation.pathname);
+        return;
+      }
+      console.log("same pathname");
+    },
+    []
+  );
   return (
     <>
       {/* <Router
@@ -167,31 +167,32 @@ export const RouterGuardDemo = () => {
       > */}
       <Router>
         <RouterGuard
-          when={whenState}
-          navigate={({ history }, nextLocation: any, curLocation: any) => {
-            if (curLocation.pathname !== nextLocation.pathname) {
-              history.push(nextLocation.pathname)
-              return
-            }
-            console.log('same pathname')
-          }}
+          when={true}
+          // navigate={({ history }, nextLocation, curLocation) => {
+          //   if (curLocation.pathname !== nextLocation.pathname) {
+          //     history.push(nextLocation.pathname);
+          //     return;
+          //   }
+          //   console.log("same pathname");
+          // }}
+          navigate={navigateCallback}
           shouldBlockNavigation={navigationBlockStrategy}
         />
         <div>
           <div>
-            <Link to='/'>Home</Link>
-            <Link to='/user'>User</Link>
-            <Link to='/private'>Private Page</Link>
+            <Link to="/">Home</Link>
+            <Link to="/user">User</Link>
+            <Link to="/private">Private Page</Link>
           </div>
           <div>
             <Switch>
-              <Route path='/' exact component={Home}></Route>
-              <Route path='/user' component={User}></Route>
-              <Route path='/private' component={Private}></Route>
+              <Route path="/" exact component={Home}></Route>
+              <Route path="/user" component={User}></Route>
+              <Route path="/private" component={Private}></Route>
             </Switch>
           </div>
         </div>
       </Router>
     </>
-  )
-}
+  );
+};
